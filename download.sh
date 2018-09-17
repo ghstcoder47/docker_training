b9f823262310bf964e2b18e4ebf33390ddc3aac8
ghstcoder47 <43176527+ghstcoder47@users.noreply.github.com>
2018-09-17 14:33:31 +0200

    download skript jdk and wfly

---
+++ download.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#download java
+wget -O java/openjdk_11.tar.gz https://download.java.net/java/early_access/alpine/28/binaries/openjdk-11+28_linux-x64-musl_bin.tar.gz
+
+#download wildfly
+wget -O jboss/wildfly-14.0.1.Final.tar.gz http://download.jboss.org/wildfly/14.0.1.Final/wildfly-14.0.1.Final.tar.gz
