32d6f175bab26910349242e03933cfc3464338c7
ghostcoder <ghostcoder47@gmail.com>
2018-09-17 21:54:26 +0200

    wildfly Docker optimierung

--- download.sh
+++ download.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 wget -O java/openjdk_11.tar.gz https://download.java.net/java/early_access/alpine/28/binaries/openjdk-11+28_linux-x64-musl_bin.tar.gz
 
 wget -O jboss/wildfly-14.0.1.Final.tar.gz http://download.jboss.org/wildfly/14.0.1.Final/wildfly-14.0.1.Final.tar.gz
